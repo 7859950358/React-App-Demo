41fd159914382eeec43be1760e777d2d6a08e013
7859950358 <mayurbhalu135@gmail.com>
2024-06-04 16:19:32 +0530

    Example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import './logo.svg';
 import './App.css';
+import img1 from './img/1.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+
+        <p className="lorem-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quaerat qui a doloribus omnis odit voluptates obcaecati voluptas? Maiores incidunt architecto ad eaque reiciendis iste commodi quae mollitia sunt! Recusandae!</p>
+
+        <img src="https://t3.ftcdn.net/jpg/05/62/54/02/360_F_562540214_unXDmpcHlTdImYhQE7dPBXZEWfFyAeeH.jpg" width="100%" />
+        <br />
+        <img src={img1} width="500px" />
       </header>
-    </div>
+    </>
   );
 }
 
