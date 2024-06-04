6242653e25c0ed536a12208d869e019012b7423d
7859950358 <mayurbhalu135@gmail.com>
2024-06-04 18:42:50 +0530

    add all file

---
+++ module 2.mjs
@@ -0,0 +1,9 @@
+const a ="Raj";
+const b ="Utsav";
+const c ="Aryan";
+const d ="Yash";
+
+export default a;
+export {b};
+export {c};
+export {d};
--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import './logo.svg';
 import './App.css';
-import img1 from './img/1.jpeg';
-let name="SIT";
+
 function App() {
   return (
     <>
-      <header>
-        <h1 className="SIT">{name}</h1>
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-
-        <p className="lorem-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quaerat qui a doloribus omnis odit voluptates obcaecati voluptas? Maiores incidunt architecto ad eaque reiciendis iste commodi quae mollitia sunt! Recusandae!</p>
-
-        <img src="https://t3.ftcdn.net/jpg/05/62/54/02/360_F_562540214_unXDmpcHlTdImYhQE7dPBXZEWfFyAeeH.jpg" width="100%" />
-        <br />
-        <img src={img1} width="500px" />
-      </header>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Raj</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
     </>
   );
 }
