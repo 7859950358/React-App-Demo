e56b93070404333e37f5395c382656b95e80f83a
7859950358 <mayurbhalu135@gmail.com>
2024-06-06 18:01:36 +0530

    save

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,8 +1,8 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
 export default function TextForm(props) {
 
-    const [text , setText]=useState("Input Your Text");
+    const [text, setText] = useState("Input Your Text");
     // setText("Enter Your Text");
 
     const handleOnChange = (event) => {
@@ -12,16 +12,46 @@ export default function TextForm(props) {
 
     const hanldeupchange = () => {
         // console.log("Btn click");
-        let newText =(text.toUpperCase());
+        let newText = (text.toUpperCase());
+        setText(newText);
+    }
+    const hanldeInchange = () => {
+        // console.log("Btn click");
+        let newText = (text.toLowerCase());
+        setText(newText);
+    }
+
+    const handleSpeak = () => {
+        let newText = new SpeechSynthesisUtterance();
+        newText.text = text;
+        window.speechSynthesis.speak(newText)
+    }
+
+    const hanldeClear = () => {
+        // console.log("Btn click");
+        let newText = ("");
         setText(newText);
     }
     return (
         <div>
-            <div className='mx-5 mb-3 my-5'>
+            <div className='container my-5'>
                 <h1>{props.heading}</h1>
                 <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleOnChange}></textarea>
-                <button className='btn btn-outline-success my-3' onClick={hanldeupchange}>convert to upper case</button>
+                <button className='btn btn-outline-success my-3' onClick={hanldeupchange}>convert to upper case</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <button className='btn btn-outline-info my-3' onClick={hanldeInchange}>convert to Lower case</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <button className='btn btn-outline-primary my-3' onClick={handleSpeak}>Speak</button>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <button className='btn btn-outline-danger my-3' onClick={hanldeClear}>clear</button>
+            </div>
+            <div className='container my-3'>
+                <h2>Your Text Summary</h2>
+                <p>{text.split(" ").length}&nbsp;&nbsp;Words</p>
+                <p>{text.length}&nbsp;&nbsp;Characters</p>
+                <p>{0.008 * text.split.length}&nbsp;&nbsp;Reading Time</p>
+                <h3>Preview</h3>
+                <p>{text}</p>
             </div>
         </div>
+
     )
 }
