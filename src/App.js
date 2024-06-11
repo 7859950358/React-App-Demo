adb09c4be78bf63ce809fc4412812eea4796e68e
7859950358 <mayurbhalu135@gmail.com>
2024-06-11 16:50:02 +0530

    save

--- src/App.js
+++ src/App.js
@@ -5,22 +5,7 @@ let name="SIT";
 function App() {
   return (
     <>
-      <header>
-        <h1 className="SIT">{name}</h1>
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-
-        <p className="lorem-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quaerat qui a doloribus omnis odit voluptates obcaecati voluptas? Maiores incidunt architecto ad eaque reiciendis iste commodi quae mollitia sunt! Recusandae!</p>
-
-        <img src="https://t3.ftcdn.net/jpg/05/62/54/02/360_F_562540214_unXDmpcHlTdImYhQE7dPBXZEWfFyAeeH.jpg" width="100%" />
-        <br />
-        <img src={img1} width="500px" />
-      </header>
+      
     </>
   );
 }
