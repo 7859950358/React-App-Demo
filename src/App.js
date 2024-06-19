8347ab2c5c61af371b03d6cae990f21b88199626
7859950358 <mayurbhalu135@gmail.com>
2024-06-19 17:58:09 +0530

    final part 1

--- src/App.js
+++ src/App.js
@@ -2,28 +2,50 @@ import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar.js';
 import TextForm from './components/TextForm.js';
+import Alert from './components/Alert.js';
 
 function App() {
-  const [mode, setMode] = useState('light')
-  const [btnText, newbtnText] = useState('Enable Dark Mode')
+  const [mode, setMode] = useState('light');
 
+  const [btnText, newbtnText] = useState('Enble Dark Mode');
+
+  const [alert, setAlert] = useState (null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark')
-      newbtnText('Enable Light Mode');
-      document.body.style.background = 'black';
-      document.body.style.color = 'white';
+      document.body.style.background = 'black'
+      document.body.style.color = 'white'
+      newbtnText("Enble Light Mode")
+      showAlert("Dark Mode Has Been Enable", "success");
+      setInterval(() => {
+        document.title=" SIt - Admission";
+      }, 2000);
+      setInterval(() => {
+        document.title=" SIt - BS.C IT";
+      }, 1500);
     } else {
-      setMode('light');
-      newbtnText('Enable Dark Mode');
-      document.body.style.background = 'white';
-      document.body.style.color = 'black';
+      setMode('light')
+      document.body.style.color = 'black'
+      document.body.style.background = 'white'
+      newbtnText("Enble Dark Mode")
+      showAlert("Light Mode Has Been Enable","success");
     }
   }
   return (
     <>
-      <Navbar title="SIT" aboutText="Contact Us" mode={mode} toggleMode={toggleMode} btnText={btnText} />
-      <TextForm heading="Enter Text To Analyse Below" mode={mode} toggleMode={toggleMode} />
+      <Navbar title="SIT" aboutText="Contact  Us" mode={mode} toggleMode={toggleMode} btnText={btnText} />
+      <Alert alert={alert}/>
+      <TextForm heading="Enter Text To Analyse Below" mode={mode} toggleMode={toggleMode} showAlert={showAlert}/>
     </>
   );
 }
